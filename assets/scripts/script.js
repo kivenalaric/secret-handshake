be491358768f44818162da769ad00766fd468238
Kiven Alaric <elmozaro3@gmail.com>
2022-12-18 06:49:35 -0600

    edits js code

---
+++ assets/scripts/script.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#element1')
+const submitBtn = document.querySelector('#submit')
+const displayBox = document.querySelector('#display')
+
+const arrayGreetings = ['wink', 'double blink', 'close your eyes', 'jump']
+
+const arrayActions = (array) => {
+  const filteredList = array.filter(item => item !== undefined)
+  if(filteredList.length === 0)
+    return display.innerHTML = `No handshakes for this number, try another`
+  filteredList.forEach((el, i, array)=>{i === (filteredList.length -1 ) ? array[i] = `${el}.`:
+array[i] = `${el}, `})
+displayBox.innerHTML = filteredList.join('').toString()
+displayBox.style.display = 'block';
+}
+
+const secretHandShake = () => {
+	let shakeFriend = [];
+	const numberToCheck = Number(Math.floor(input.value)).toString(2);
+		if (isNaN(numberToCheck) || numberToCheck < 0) return display.innerHTML = 'Please put in a number greater than 0';
+		const newArray = numberToCheck.toString().split('');
+		const reverseArray = newArray.reverse();
+    reverseArray.forEach((el, i)=>{
+        if (el === '1')shakeFriend.push(arrayGreetings[i]);
+		})
+		if (reverseArray[4] === '1')shakeFriend.reverse();
+	arrayActions(shakeFriend);
+}
+
+submitBtn.addEventListener('click', secretHandShake);
