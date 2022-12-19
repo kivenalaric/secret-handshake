cb3ba76422374b03b0968e290562f681ddfe6e91
Kiven Alaric <elmozaro3@gmail.com>
2022-12-19 05:45:40 -0600

    edits on js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,31 +1,104 @@
-const input = document.querySelector('#element1')
-const submitBtn = document.querySelector('#submit')
+const inputBox = document.getElementById('element1')
 const displayBox = document.querySelector('#display')
+const submitBtn = document.querySelector('#submit')
 
-const arrayGreetings = ['wink', 'double blink', 'close your eyes', 'jump']
+const arrayOfCode = [
+  { bin: 1, response: 'wink', deci: 1 },
+  { bin: 10, response: 'double blink', deci: 2 },
+  { bin: 11, response: 'wink, double blink', deci: 3 },
+  { bin: 100, response: 'close your eyes', deci: 4 },
+  { bin: 1000, response: 'jump', deci: 8 }
+]
 
-const arrayActions = (array) => {
-  const filteredList = array.filter(item => item !== undefined)
-  if (filteredList.length === 0) { return (displayBox.innerHTML = ('No handshakes for this number, try another')) }
-  filteredList.forEach((el, i, array) => {
-    i === (filteredList.length - 1)
-      ? array[i] = `${el}.`
-      : array[i] = `${el}, `
-  })
-  displayBox.innerHTML = filteredList.join('').toString()
+const reversedArray = [
+  { bin: 1000, response: 'jump', deci: 8 },
+  { bin: 100, response: 'close your eyes', deci: 4 },
+  { bin: 11, response: 'double blink, wink', deci: 3 },
+  { bin: 10, response: 'double blink', deci: 2 },
+  { bin: 1, response: 'wink', deci: 1 }
+]
+
+let k = 0 // k counts the number of times findResponse is called and chooses to add a comma and space
+let revert = false
+
+const convertToBinary = (val) => {
+  let remainder = 0
+  let binary = 0
+  let i = 1
+  while (val !== 0) {
+    remainder = val % 2
+    val = parseInt(val / 2)
+    binary += remainder * i
+    i *= 10
+  }
+  return binary
 }
 
-const secretHandShake = () => {
-  const shakeFriend = []
-  const numberToCheck = Number(Math.floor(input.value)).toString(2)
-  if (isNaN(numberToCheck) || numberToCheck < 0) return (displayBox.innerHTML = ('Please put in a number greater than 0'))
-  const newArray = numberToCheck.toString().split('')
-  const reverseArray = newArray.reverse()
-  reverseArray.forEach((el, i) => {
-    if (el === '1')shakeFriend.push(arrayGreetings[i])
-  })
-  if (reverseArray[4] === '1')shakeFriend.reverse()
-  arrayActions(shakeFriend)
+const findResponse = (val, arr) => {
+  const binVal = convertToBinary(val)
+  console.log(binVal)
+  for (let i = 0; i < arr.length; i++) {
+    if (binVal === arr[i].bin) { // calling the fxn to convert to binary
+      displayBox.innerHTML += `${k > 0 ? ', ' : ''}${arr[i].response}`
+    }
+  }
 }
 
-submitBtn.addEventListener('click', secretHandShake)
+const checkRange = (valu, arre) => {
+  if ((valu <= 4) || (valu === 8)) {
+    findResponse(valu, arre)
+  } else if ((valu > 4) && (valu < 8)) {
+    if (revert === false) {
+      findResponse(valu - 4, arre)
+      k++
+      findResponse(4, arre)
+    } else {
+      findResponse(4, arre)
+      k++
+      findResponse(valu - 4, arre)
+    }
+  } else if ((valu > 8) && (valu < 16)) {
+    if (valu < 13) {
+      if (revert === false) {
+        findResponse(valu - 8, arre)
+        k++
+        findResponse(8, arre)
+      } else {
+        findResponse(8, arre)
+        k++
+        findResponse(valu - 8, arre)
+      }
+    } else {
+      if (revert === false) {
+        findResponse(valu - 12, arre)
+        k++
+        findResponse((valu - (valu - 12)) - 8, arre)
+        k++
+        findResponse(8, arre)
+      } else {
+        findResponse(8, arre)
+        k++
+        findResponse((valu - (valu - 12)) - 8, arre)
+        k++
+        findResponse(valu - 12, arre)
+      }
+    }
+  }
+  k = 0
+}
+
+submitBtn.addEventListener('click', () => {
+  displayBox.innerHTML = ''
+  if (inputBox.value < 16) {
+    checkRange(inputBox.value, arrayOfCode)
+  } else if (inputBox.value >= 16) {
+    if (inputBox.value === 16) {
+      displayBox.innerHTML += '! secret Code reversed'
+    } else {
+      revert = true
+      checkRange((inputBox.value - 16), reversedArray)
+      console.log(revert)
+    }
+  }
+  revert = false
+})
